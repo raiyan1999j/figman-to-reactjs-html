0016786e3e9b74cb3d51146c04a526b44c3df1b9
raiyan1999j <62192284+raiyan1999j@users.noreply.github.com>
2024-11-15 12:32:16 +0600

    header - navbar sticky

--- resturant/src/ClientSide/Header/Navbar.jsx
+++ resturant/src/ClientSide/Header/Navbar.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 import Logo from "../../../public/logoImg.png";
 
 export default function Navbar(){
+    const [scrollDetection,setScrollDetect] = useState(0);
+
+    useEffect(()=>{
+        const handleScroll=()=>{
+            setScrollDetect(window.scrollY)
+        }
+
+        window.addEventListener("scroll",handleScroll);
+
+        return ()=>{
+            window.removeEventListener("scroll",handleScroll)
+        }
+    },[])
     return(
         <>
-            <section className="w-full mx-auto relative bg-bannerBg before:content-'' before:table before:absolute before:w-full before:bg-[#BD1F17]/90 before:h-full before:bg-cover before:-z-30">
+            <section className={`w-full mx-auto ${scrollDetection>10?
+            "fixed top-0 h-[108px] w-full bg-[#BD1F17]/90 z-50 before:content-'' before:table before:bg-bannerBg before:absolute before:inset-0 before:h-full before:w-full before:-z-30":
+            "relative bg-bannerBg before:content-'' before:table before:absolute before:w-full before:bg-[#BD1F17]/90 before:h-full before:bg-cover before:-z-30"}`}>
                 <nav className="w-[1320px] h-[108px] mx-auto flex flex-row items-center text-white">
                     <div className="flex flex-row items-center">
                         <span className="w-[36.99px] h-[37px] mr-[5.99px]">
@@ -16,12 +32,12 @@ export default function Navbar(){
 
                     <div className="ml-[60px]">
                         <ul className="capitalize font-raleway font-medium text-[15px] leading-[17.61px] flex flex-row gap-x-10">
-                            <li>home</li>
-                            <li>about</li>
-                            <li>portfolio</li>
-                            <li>clients</li>
-                            <li>blog</li>
-                            <li>contact</li>
+                            <li className=" hover:cursor-pointer">home</li>
+                            <li className=" hover:cursor-pointer">about</li>
+                            <li className=" hover:cursor-pointer">portfolio</li>
+                            <li className=" hover:cursor-pointer">clients</li>
+                            <li className=" hover:cursor-pointer">blog</li>
+                            <li className=" hover:cursor-pointer">contact</li>
                         </ul>
                     </div>
 
